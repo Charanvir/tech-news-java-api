385f97b528aaaa33afdb014dccbf285afedc38bf
Charanvir <charanvir123@gmail.com>
2022-08-03 03:18:25 -0400

    built entire structure of database

---
+++ tech-news-java-api/src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,17 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args){
+        int a = 5;
+        double b = 3.75;
+        String c = "Charanvir";
+
+        double sum = a + b;
+
+        String concat = a + c;
+
+        System.out.println(sum);
+
+        System.out.println(concat);
+    }
+}
